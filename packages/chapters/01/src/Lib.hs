79ef416c2a7eec1a0cef8e2e40f0c321cdd4ccfb
Artemis Meursault Gerrard <agerrard@meddbase.com>
2022-09-08 09:25:40 +0000

    Chapter 1.5: sum
    hlint suggested a bunch of things, I followed the rabbit hole a bit; hence the foldr implementations.

--- packages/chapters/01/src/Lib.hs
+++ packages/chapters/01/src/Lib.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 module Lib
     ( someFunc
     ) where
@@ -5,9 +7,29 @@ module Lib
 double :: Integer -> Integer
 double x = x + x
 
+{-# HLINT summm supersum #-}
+summm :: [Integer] -> Integer
+summm = foldr (+) 0
+
+{-# HLINT ignore supersum #-}
+supersum :: [Integer] -> Integer
+supersum restoflist = foldr (+) 0 restoflist
+
+superdupersum :: [Integer] -> Integer
+superdupersum [] = 0
+superdupersum (first:restoflist) = first + supersum restoflist
+
 someFunc :: IO ()
 someFunc = do
     putStr "double 4: "
     print (double 4)
     putStr "double (double 4): "
     print (double (double 4))
+    putStr "sum [1..5]: "
+    print (sum [1..5 :: Integer])
+    putStr "summm [1..5]: "
+    print (summm [1..5])
+    putStr "supersum [1..5]: "
+    print (supersum [1..5])
+    putStr "superdupersum [1..5]: "
+    print  (superdupersum [1..5])
