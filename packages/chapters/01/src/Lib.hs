46cc2537029947e7a1d4660348ea23104330ac85
Artemis Meursault Gerrard <agerrard@meddbase.com>
2022-09-08 09:12:21 +0000

    Chapter 1.1: double

--- packages/chapters/01/src/Lib.hs
+++ packages/chapters/01/src/Lib.hs
@@ -2,5 +2,12 @@ module Lib
     ( someFunc
     ) where
 
+double :: Integer -> Integer
+double x = x + x
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    putStr "double 4: "
+    print (double 4)
+    putStr "double (double 4): "
+    print (double (double 4))
